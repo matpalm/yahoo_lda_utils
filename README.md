e5296bb920ed8453c39d3f4ec8b0542e465b1454
mat kelcey <matthew.kelcey@gmail.com>
2012-05-12 15:02:54 -0700

    lda_prep

---
+++ README.md
@@ -0,0 +1,31 @@
+# Intro
+
+[Yahoo LDA](https://github.com/shravanmn/Yahoo_LDA) is my fav [LDA](http://en.wikipedia.org/wiki/Latent_Dirichlet_allocation) implementation at the moment. 
+
+I've found though to understand the topics it generates it's useful to have some helper scripts, like these...
+
+# Dependencies
+
+These helpers are a mix of python and c++. I wrote everything in python but then rewrote the slow ones in c++. The c++ ones build with cmake and depend on boost.
+
+    cmake .
+    make # builds straight into bin
+
+# Input helpers
+
+The input format for YahooLDA is
+
+    $ cat documents
+    primary_id secondary_id token token token ...
+
+To do a simple normalisation (downcase and remove all tokens with len < 3 and without at least one alpha numeric)
+
+    $ cat documents | bin/lda_prep > documents.normalised
+
+
+
+
+   
+
+
+
---
+++ src/lda_prep.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+bool has_at_least_one_alpha_numeric(string &token) {
+    for(string::iterator it=token.begin(); it!=token.end(); ++it)
+        if (isalnum(*it))
+            return true;
+    return false;
+}
+
+int main(int argc, char **argv) {
+    string line;
+    string token;
+    while (getline(cin, line)) {
+        // tokenise line
+        istringstream iss(line, istringstream::in);
+        // read and write primary_id & secondary_id
+        iss >> token;
+        cout << token;
+        iss >> token;
+        cout << " " << token;
+        while (iss >> token) {
+            // ignore short strings
+            if (token.length() < 3)
+                continue;
+            // must have at least one alpha
+            if (!has_at_least_one_alpha_numeric(token))
+                continue;
+            // downcase
+            boost::algorithm::to_lower(token);
+            // emit
+            cout << " " << token;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
