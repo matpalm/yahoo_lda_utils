d7e973a619f5a3bff4cb4113f9e43a372b054a43
Manish Katyal <manish@wavii.com>
2012-05-16 16:31:05 -0700

    filter out the tail - if required

--- bin/mass_per_topic.py
+++ bin/mass_per_topic.py
@@ -5,13 +5,20 @@
 import sys
 from collections import defaultdict
 import docToTop
+from optparse import OptionParser
+
+usage = "usage: cat %prog [options] < lda.docToTop.txt"
+parser = OptionParser(usage)
+parser.add_option("-p", "--min_prob", dest="min_prob", default=0.0, help="minimum probability")
+(options, _) = parser.parse_args()
 
 total_mass = defaultdict(float)
 
 for line in sys.stdin: 
     id1, id2, topic_probs = docToTop.parse(line)
     for topic, prob in topic_probs:
-        total_mass[topic] += prob
+        if float(prob) >= float(options.min_prob):
+            total_mass[topic] += prob
 
 for k, v in total_mass.iteritems():
     print "%s\t%s" % (k,v)
