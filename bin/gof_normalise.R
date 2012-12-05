60f7062f8516453b0a751571d77f0742ec507b99
mat kelcey <matthew.kelcey@gmail.com>
2012-12-04 23:01:28 -0800

    switch to apply

--- bin/gof_normalise.R
+++ bin/gof_normalise.R
@@ -4,18 +4,10 @@ library(NCStats)
 setwd("/home/mat/dev/yahoo_lda_utils/bin")
 df = read.delim('foo.tsv', h=F, col.names=c('id','A','B','C','D','E'))
 
-for(i in 1:nrow(df)) {
-  row = df[i,]
-  item_id = row$id
-  topics = row[2:5]
+calc_ci_lower = function(topics) {
   chi.sq = chisq.test(topics)
   fit = gofCI(chi.sq, conf.level=0.99)
-  ci_lower = fit[,2]
-  print(item_id)
-  print(topics)
-  print(ci_lower)
-  residual = 1 - sum(ci_lower)
-  print(residual)
+  fit[,2]
 }
 
-df[1,]
+t(apply(df[,2:6], 1, calc_ci_lower))
